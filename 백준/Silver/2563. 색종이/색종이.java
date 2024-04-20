c34138c91685ae96397ffa4002174187693e39fd
김지연 <90545043+kdelay@users.noreply.github.com>
2024-04-20 15:43:23 +0900

    [Silver V] Title: 색종이, Time: 120 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,11 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
