21f6ececba1808b13159b4f69b991c8b3a021a22
김지연 <90545043+kdelay@users.noreply.github.com>
2024-04-25 14:17:46 +0900

    [Silver IV] Title: 설탕 배달, Time: 124 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -3,18 +3,24 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int N = Integer.parseInt(br.readLine());
 
-    if (N == 4 || N == 7) {
-      System.out.println(-1);
-    } else if (N % 5 == 0) {
-      System.out.println(N / 5);
-    } else if (N % 5 == 1 || N % 5 == 3) {
-      System.out.println((N / 5) + 1);
-    } else if (N % 5 == 2 || N % 5 == 4) {
-      System.out.println((N / 5) + 2);
+    int result = 0;
+    while (N % 5 != 0) { // 5kg 봉지에 담을 수 없는 경우
+      N -= 3; // 3kg에 모두 담기
+      result++;
     }
+
+    if (N < 0) { // Nkg를 완벽하게 담을 수 없는 경우
+      System.out.println(-1);
+      return;
+    } else {
+      // 5kg에 담을 수 있는 경우 모두 담기
+      result += (N / 5);
+    }
+    System.out.println(result);
   }
 }
