6e82698bd959d8302fd61dafb70b4c5de4cd990a
김지연 <90545043+kdelay@users.noreply.github.com>
2024-05-01 19:56:57 +0900

    [Gold V] Title: LCS, Time: 156 ms, Memory: 18268 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -3,37 +3,27 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException{
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      char[] a = br.readLine().toCharArray();
+      char[] b = br.readLine().toCharArray();
 
-    char[] str1 = br.readLine().toCharArray();
-    char[] str2 = br.readLine().toCharArray();
+      int lenA = a.length;
+      int lenB = b.length;
+      int[][] dp = new int[lenA+1][lenB+1];
 
-    int length1 = str1.length;
-    int length2 = str2.length;
-
-    //공집합을 위한 배열 길이 1 추가
-    int[][] dp = new int[length1+1][length2+1];
-
-    //공집합 빼고 1부터 시작
-    for (int i=1; i<=length1; i++) {
-      for (int j=1; j<=length2; j++) {
-
-        //(i-1), (j-1) 문자가 같다면
-        if (str1[i-1] == str2[j-1]) {
-          //대각선 위(i-1, j-1)의 dp에 +1 한 값으로 갱신
-          dp[i][j] = dp[i-1][j-1] + 1;
-        }
-
-        //같지 않다면 이전 열(i-1), 이전 행(j-1) 중에 max값으로 갱신
-        else {
-          dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+      for (int i=1; i<=lenA; i++) {
+        for (int j=1; j<=lenB; j++) {
+          if (a[i-1] == b[j-1]) { //동일한 문자일 경우 대각선 값+1 저장
+            dp[i][j] = dp[i-1][j-1] + 1;
+          } else { //다른 문자일 경우 상좌 데이터 중 큰 수 저장
+            dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+          }
         }
       }
+      System.out.println(dp[lenA][lenB]);
     }
-    System.out.println(dp[length1][length2]);
-  }
 }
