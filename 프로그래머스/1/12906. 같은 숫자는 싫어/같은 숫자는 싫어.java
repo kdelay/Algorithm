ecb30b6c1faaa5425d4b8b564b0f10ea08355d35
김지연 <90545043+kdelay@users.noreply.github.com>
2024-06-04 12:52:45 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 34.23 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -1,25 +1,15 @@
 import java.util.*;
 
 public class Solution {
-    /**
-     * @param arr 배열
-     * @return 중복 숫자 없이 배열 반환
-     */
-    public int[] solution(int []arr) {
-        
+        public Stack<Integer> solution(int []arr) {
+
         Stack<Integer> stack = new Stack<>();
 
         for (int a : arr) {
-            if (stack.isEmpty()) {
+            if (stack.isEmpty() || stack.peek() != a) {
                 stack.push(a);
-            } else {
-                if (stack.peek() != a) {
-                    stack.push(a);
-                }
             }
         }
-        return stack.stream()
-                .mapToInt(Integer::intValue)
-                .toArray();
+        return stack;
     }
 }
