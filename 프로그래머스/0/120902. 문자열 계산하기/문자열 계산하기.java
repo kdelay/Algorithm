0833580db110eb92c0cff3b7a3325bbfe2a8f183
김지연 <90545043+kdelay@users.noreply.github.com>
2024-06-04 14:58:26 +0900

    [level 0] Title: 문자열 계산하기, Time: 1.91 ms, Memory: 89.7 MB -BaekjoonHub

--- 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.java
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.java
@@ -1,26 +1,13 @@
-import java.util.Stack;
+import java.util.Arrays;
 
 class Solution {
     public int solution(String my_string) {
         
-        String[] str = my_string.split(" ");
-        int result = Integer.parseInt(str[0]);
-        
-        boolean oper = true;
-        
-        for (int i = 1; i < str.length; i++) {
-            
-            if (str[i].equals("-")) oper = false;
-            else if (str[i].equals("+")) oper = true;
-            else {
-                if (oper) { //+
-                    result += Integer.parseInt(str[i]);
-                } else { //-
-                    result -= Integer.parseInt(str[i]);
-                }
-            }
-        }
-        
-        return result;
+        return Arrays.stream(
+                my_string.replaceAll("- ", "-")
+                        .replaceAll("[+] ", "")
+                        .split(" "))
+                .mapToInt(Integer::parseInt)
+                .sum();
     }
 }
