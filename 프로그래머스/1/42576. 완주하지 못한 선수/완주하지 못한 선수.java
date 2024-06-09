e6c63f53b9c38f023c157973c0589f0e6fd14fc3
김지연 <90545043+kdelay@users.noreply.github.com>
2024-06-09 16:44:14 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 79.42 ms, Memory: 95.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -7,31 +7,17 @@ class Solution {
      * @return 완주하지 못한 선수 이름
      */
     public String solution(String[] participant, String[] completion) {
-
-        HashMap<String, Integer> map = new HashMap<>();
-
-        //map 초기화
-        for (String k : participant) {
-            if (map.containsKey(k)) {
-                int value = map.get(k) + 1;
-                map.replace(k, value);
-            } else {
-                map.put(k, 1);
-            }
-        }
         
-        for (String k : completion) {
-            int value = map.get(k) - 1;
-            map.replace(k, value);
+        String mem = "";
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for (String p : participant) map.put(p, map.getOrDefault(p, 0) + 1);
+        for (String p : completion) map.put(p, map.get(p) - 1);
+
+        for (String k : map.keySet()) {
+            if (map.get(k) != 0) mem = k;
         }
 
-        //value가 0 이상이면 완주하지 못한 선수이다.
-        String mem = "";
-        for (String k : map.keySet()) {
-            if (map.get(k) > 0) {
-                mem = k;
-            }
-        }
         return mem;
     }
 }
