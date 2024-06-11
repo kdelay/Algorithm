d792b0a3eeebefcfc733e756d042efa7f8a0d2db
김지연 <90545043+kdelay@users.noreply.github.com>
2024-06-11 15:30:06 +0900

    [level 2] Title: 오픈채팅방, Time: 113.08 ms, Memory: 150 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -1,50 +1,33 @@
 import java.util.*;
 
 class Solution {
-    /**
+        /**
      * @param record 채팅방에 들어오고 나가거나, 닉네임을 변경한 기록이 담긴 문자열 배열
      * @return 최종적으로 방을 개설한 사람이 보게 되는 메시지
      */
     public static String[] solution(String[] record) {
 
-        int len = record.length;
+        HashMap<String, String> msg = new HashMap<>();
+        msg.put("Enter", "님이 들어왔습니다.");
+        msg.put("Leave", "님이 나갔습니다.");
 
-        //유저 아이디, 닉네임 담을 map
-        Map<String, String> map = new HashMap<>();
-        //명령어, 유저 아이디 담을 list
-        List<String[]> list = new ArrayList<>();
+        HashMap<String, String> uid = new HashMap<>();
 
-        for (int i = 0; i < len; i++) {
-            StringTokenizer st = new StringTokenizer(record[i]);
-            String cmd = st.nextToken();
-            String user_id = st.nextToken();
-
-            switch (cmd) {
-                case "Enter":
-                    map.put(user_id, st.nextToken());
-                    list.add(new String[]{cmd, user_id});
-                    break;
-                case "Leave":
-                    list.add(new String[]{cmd, user_id});
-                    break;
-                case "Change":
-                    map.replace(user_id, st.nextToken());
-                    break;
+        for (String s : record) {
+            String[] cmd = s.split(" ");
+            if (cmd.length == 3) { //Enter 또는 Change인 경우
+                uid.put(cmd[1], cmd[2]); //명령어, user id
             }
         }
 
-        List<String> result = new ArrayList<>();
+        ArrayList<String> answer = new ArrayList<>();
 
-        for (String[] li : list) {
-            String cmd = li[0];
-            String user_id = li[1];
-
-            if (cmd.equals("Enter")) {
-                result.add(map.get(user_id) + "님이 들어왔습니다.");
-            } else {
-                result.add(map.get(user_id) + "님이 나갔습니다.");
+        for (String s : record) {
+            String[] cmd = s.split(" ");
+            if (msg.containsKey(cmd[0])) {
+                answer.add(uid.get(cmd[1]) + msg.get(cmd[0]));
             }
         }
-        return result.toArray(new String[result.size()]);
+        return answer.toArray(new String[0]);
     }
 }
