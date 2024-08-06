434a68e61e42d1dcaf37e54b010803608495c49e
김지연 <90545043+kdelay@users.noreply.github.com>
2024-08-06 12:39:09 +0900

    Update 1로 만들기.java

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,22 +1,20 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Main {
 
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    long N = Long.parseLong(br.readLine());
-    System.out.println(dynamic(N, 0));
-  }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long x = sc.nextLong();
+        System.out.println(dp(0, x));
+    }
 
-  private static int dynamic(long N, int count) {
-    // 1인 경우
-    if (N < 2) return count;
+    private static int dp(int cnt, long x) {
+        //재귀 종료 조건
+        if (x < 2) return cnt;
 
-    return Math.min(
-        dynamic(N/2, (int) (count+1+(N%2))),
-        dynamic(N/3, (int) (count+1+(N%3)))
-    );
-  }
+        //2와 3으로 나누어지지 않는 경우, 1을 빼야 하기 때문에 % 모듈러 연산 개수 추가
+        //3으로 나눌 경우: 10(1) -> 3 -> 1 (cnt:2) + x%3 (cnt:1) = cnt:3
+        //2로 나눌 경우: 10 -> 5(1) -> 2 -> 1 (cnt:3) + x%2 (cnt:1) = cnt:4
+        return Math.min(dp((int)(cnt+1+(x%3)), x/3), dp((int)(cnt+1+(x%2)), x/2));
+    }
 }
