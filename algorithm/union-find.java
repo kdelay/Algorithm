bc927cf8043b7e10d4a1eb2c13d02aca15ea87c2
김지연 <90545043+kdelay@users.noreply.github.com>
2024-08-20 15:23:47 +0900

    Update union-find.java

--- algorithm/union-find.java
+++ algorithm/union-find.java
@@ -1,6 +1,6 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 
 //--- Union-Find Algorithm
 public class Solution {
@@ -10,48 +10,46 @@ public class Solution {
 
     //0 -> union, 1 -> equals 연산
 
-    static int[] nodes;
+    private static int[] nodes;
 
     public static void main(String[] args) {
-        int k = 3;
-        int[][] operations = {{0,0,1}, {0,1,2}, {1,1,2}};
-//        int[][] operations = {{0,0,1},{1,1,2},{0,1,2},{1,0,2}};
+        int k = 4;
+//        int[][] operations = {{0,0,1}, {0,1,2}, {1,1,2}};
+        int[][] operations = {{0,0,1},{1,1,2},{0,1,2},{1,0,2}};
+        System.out.println(Arrays.toString(solution(k, operations)));
+    }
 
+    public static Boolean[] solution(int k, int[][] operations) {
         //초기 노드
         nodes = new int[k];
         for (int i = 0; i < nodes.length; i++) {
             nodes[i] = i;
         }
 
-        List<Boolean> result = Arrays.stream(operations)
-                .map(op -> solution(op[0], op[1], op[2]))
-                .filter(Objects::nonNull)
-                .toList();
-        System.out.println(result);
+        List<Boolean> result = new ArrayList<>();
+        for (int[] operation : operations) {
+            int op = operation[0];
+            int x = operation[1], y = operation[2];
+
+            if (op == 0) union(x, y);
+            else result.add(find(x) == find(y));
+        }
+        return result.toArray(new Boolean[0]);
     }
 
-    public static Boolean solution(int node, int x, int y) {
-        switch (node) {
-            case 0:
-                union(x, y);
-                return null;
-            case 1:
-                return equals(x, y);
-        }
-        return null;
+    private static int find(int x) {
+        //자신이 루트 노드일 경우 반환
+        if (nodes[x] == x) return x;
+        //루트 노드 찾기
+        return find(nodes[x]);
     }
 
     private static void union(int x, int y) {
-        if (x < y) nodes[x] = y;
-        else nodes[y] = x;
-    }
-
-    private static boolean equals(int x, int y) {
-        //종료 조건
-        if (nodes[x] == y) return true; //부모 노드일 경우 같은 집합
-        if (x == nodes[x]) return false; //루트 노드일 경우 다른 집합
-
-        //루트 노드까지 재귀 진행
-        return equals(nodes[x], y);
+        //x가 속한 집합의 루트 노드 찾기
+        int rootX = find(x);
+        //y가 속한 집합의 루트 노드 찾기
+        int rootY = find(y);
+        //작은 노드를 루트 노드로 연결
+        nodes[rootY] = rootX;
     }
 }
