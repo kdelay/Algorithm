578932316bfede1231cf482f9dad8c5f8d00ec02
김지연 <90545043+kdelay@users.noreply.github.com>
2024-08-20 15:39:05 +0900

    [level 1] Title: 폰켓몬, Time: 18.14 ms, Memory: 93.6 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -4,12 +4,9 @@ import java.util.stream.Collectors;
 
 class Solution {
     public int solution(int[] nums) {
-Set<Integer> set = Arrays.stream(nums)
+        Set<Integer> set = Arrays.stream(nums)
                 .boxed()
                 .collect(Collectors.toSet());
-        int having = nums.length / 2;
-        //가져갈 수 있는 폰켓몬 수보다 더 많은 종류를 가져갈 수 없다.
-        if (set.size() > having) return having;
-        return set.size();
+        return Math.min(set.size(), nums.length / 2);
     }
 }
