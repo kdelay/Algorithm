c42b812fd9fc3a671dd28b9401335247f7985e73
김지연 <90545043+kdelay@users.noreply.github.com>
2024-08-21 15:29:09 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.13 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -5,39 +5,22 @@ import java.util.Set;
 class Solution {
     public int[] solution(int n, String[] words) {
 
-        int[] result = new int[2];
         Set<String> set = new HashSet<>();
-        set.add(words[0]);
+        char preWord = words[0].charAt(0);
 
-        for (int i = 1; i < words.length; i++) {
+        for (int i = 0; i < words.length; i++) {
 
-            //사람 번호
-            int person = (i % n) + 1;
-            //차례 번호
-            int count = (i / n) + 1;
-
-            String before = words[i - 1];
-            String now = words[i];
-
-            //이미 나온 단어를 말한 경우
-            if (set.contains(now)) {
-                result[0] = person;
-                result[1] = count;
-                return result;
+            //이미 나온 단어를 말하거나 잘못 이어 말한 경우
+            if (set.contains(words[i]) || words[i].charAt(0) != preWord) {
+                return new int[]{(i % n) + 1, (i / n) + 1};
             }
-
-            set.add(now);
-            int index = before.length() - 1;
-            char first = now.charAt(0);
-            char last = before.charAt(index);
-
-            //잘못 이어 말한 경우
-            if (last != first) {
-                result[0] = person;
-                result[1] = count;
-                return result;
-            }
+            
+            //사용 단어 출가
+            set.add(words[i]);
+            
+            //마지막 글자 업데이트
+            preWord = words[i].charAt(words[i].length() - 1);
         }
-        return result;
+        return new int[]{0, 0};
     }
 }
