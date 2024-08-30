b87e1786fa31e9ecd6d74066cc7c9910657bf8d4
김지연 <90545043+kdelay@users.noreply.github.com>
2024-08-30 14:27:44 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.40 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] solution(String[] wallpaper) {
+        
+        int lux = 51, luy = 51, rdx = 0, rdy = 0;
+        
+        for (int i = 0; i < wallpaper.length; i++) {
+            for (int j = 0; j < wallpaper[i].length(); j++) {
+                
+                char c = wallpaper[i].charAt(j);
+                
+                if (c == '#') {
+                    lux = Math.min(i, lux);
+                    luy = Math.min(j, luy);
+                    rdx = Math.max(i, rdx);
+                    rdy = Math.max(j, rdy);
+                }
+            }
+        }
+        int[] answer = {lux, luy, rdx+1, rdy+1};
+        return answer;
+    }
+}
