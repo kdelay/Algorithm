31fa2cbd6d9847bde8e6ac80e8776a0023a7f01e
김지연 <90545043+kdelay@users.noreply.github.com>
2024-09-06 00:27:47 +0900

    [level 0] Title: 다항식 더하기, Time: 7.50 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.java
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.java
@@ -1,23 +1,16 @@
 class Solution {
     public String solution(String polynomial) {
         
-        String[] data = polynomial.replaceAll(" ", "").split("\\+");
-        
         int xCnt = 0, valCnt = 0;
-        for (String d : data) {
-            if (d.contains("x")) {
-                if (d.length() > 1) xCnt += Integer.parseInt(d.split("x")[0]);
-                else xCnt++;
+        for (String poly : polynomial.split(" ")) {
+            if (poly.contains("x")) {
+                xCnt += (poly.equals("x")) ? 1 : Integer.parseInt(poly.replaceAll("x", ""));    
             }
-            else valCnt += Integer.parseInt(d);
+            else if (!poly.contains("+")) {
+                valCnt += Integer.parseInt(poly);
+            }
         }
-        
-        String answer;
-        if (xCnt > 0) {
-            answer = (xCnt != 1) ? (xCnt + "x") : "x";
-            if (valCnt > 0) answer += (" + " + valCnt);
-        }
-        else answer = valCnt+"";
-        return answer;
+        return ((xCnt > 0) ? (xCnt != 1) ? (xCnt + "x") : "x" : "") + 
+            ((valCnt > 0) ? (xCnt > 0) ? (" + " + valCnt) : (valCnt + "") : "");
     }
 }
