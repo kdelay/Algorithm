522d714911daca7138b92917bd8f14548668f442
김지연 <90545043+kdelay@users.noreply.github.com>
2024-09-05 16:18:13 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 25.94 ms, Memory: 96.6 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -4,56 +4,38 @@ class Solution {
     public int solution(String[] friends, String[] gifts) {
         
         Map<String, Integer> name = new HashMap<>();
-        Map<Integer, int[]> share = new HashMap<>();
         
         //초기 데이터
-        int idx = 0;
-        for (String friend : friends) {
-            name.put(friend, idx++);
-            share.put(name.get(friend), new int[]{0, 0});
+        for (int i = 0; i < friends.length; i++) {
+            name.put(friends[i], i);
         }
         
-        int[][] count = new int[name.size()][name.size()];
-        boolean[][] visited = new boolean[name.size()][name.size()];
+        int[] share = new int[friends.length];
+        int[][] count = new int[friends.length][friends.length];
+        
         for (String gift : gifts) {
             int give = name.get(gift.split(" ")[0]);
             int take = name.get(gift.split(" ")[1]);
             
-            //주고 받은 선물 개수(선물 지수 계산에 사용)
-            share.get(give)[0]++;
-            share.get(take)[1]++;
+            //선물 지수 계산
+            share[give]++;
+            share[take]--;
             
             //누구한테 선물 받았는지
             count[give][take]++;
         }
         
-        //선물 지수 계산
-        Map<Integer, Integer> state = new HashMap<>();
-        for (String n : name.keySet()) {
-            int value = share.get(name.get(n))[0] - share.get(name.get(n))[1];
-            state.put(name.get(n), value);
-        }
-        
-        int[] result = new int[name.size()];
+        int answer = 0;
         for (int i = 0; i < name.size(); i++) {
+            int tmp = 0; //인당 얼마나 선물을 받았는지 계산 변수
             for (int j = 0; j < name.size(); j++) {
                 if (i == j) continue;
                 
-                if (!visited[i][j] && !visited[j][i]) {
-                    if (count[i][j] > count[j][i]) result[i]++;
-                    else if (count[i][j] < count[j][i]) result[j]++;
-                    //동일한 경우 선물 지수 비교
-                    else {
-                        if (state.get(i) > state.get(j)) result[i]++;
-                        else if (state.get(i) < state.get(j)) result[j]++;
-                    }    
-                }
-                visited[i][j] = true; visited[j][i] = true;
+                if (count[i][j] > count[j][i]) tmp++;
+                //동일한 경우 선물 지수 비교
+                else if (count[i][j] == count[j][i] && share[i] > share[j]) tmp++;
             }
-        }
-        int answer = 0;        
-        for (int r : result) {
-            answer = Math.max(answer, r);
+            answer = Math.max(answer, tmp);
         }
         return answer;
     }
