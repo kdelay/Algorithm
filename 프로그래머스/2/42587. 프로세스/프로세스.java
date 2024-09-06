33085149d27351422f6c8d9bbdc4e20cf40d5fd6
김지연 <90545043+kdelay@users.noreply.github.com>
2024-09-06 23:13:13 +0900

    [level 2] Title: 프로세스, Time: 1.21 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -3,48 +3,28 @@ import java.util.*;
 class Solution {
     public int solution(int[] priorities, int location) {
         
-        Queue<Integer> queue = new LinkedList<>();
-        List<Integer> compare = new ArrayList<>();
-        Queue<Integer> marking = new LinkedList<>();
+        //우선 순위 큐
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
         
         //초기 값
-        for (int i = 0; i < priorities.length; i++) {
-            marking.add((i == location) ? 1 : 0);
-            queue.add(priorities[i]);
-            compare.add(priorities[i]);
+        for (int p : priorities) {
+            queue.add(p);
         }
         
-        int count = 0, idx = 0;
-        
-        while (true) {
-            //기준 값
-            int cur = queue.poll();
-            boolean tf = false;
-
-            //우선 순위가 밀리는 것이 있는지 확인
-            for (int j = 1; j < queue.size(); j++) {
-                if (cur < compare.get(j)) {
-                    tf = true;
-                    break;
+        //큐가 빌 때까지 진행
+        int answer = 0;
+        while (!queue.isEmpty()) {
+            for (int i = 0; i < priorities.length; i++) {
+                //동일한 값인 경우만 제거 가능
+                if (queue.peek() == priorities[i]) {
+                    queue.poll();
+                    answer++; //실행 횟수 추가
+                    
+                    //target 번호와 동일할 경우 종료
+                    if (location == i) return answer;
                 }
             }
-            //우선 순위가 밀리면 다시 큐에 넣는다.
-            if (tf) {
-                queue.add(cur);
-                    
-                compare.remove(0);
-                compare.add(cur);
-
-                int tmp = marking.poll();
-                marking.add(tmp);
-            }
-            //실행할 수 있는 경우
-            else {
-                compare.remove(0);
-                count++;
-                //target 인 경우 종료
-                if (marking.poll() == 1) return count;
-            }
         }
+        return answer;
     }
 }
