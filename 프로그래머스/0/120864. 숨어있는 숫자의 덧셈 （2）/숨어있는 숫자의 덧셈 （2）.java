c7bb8bdb1bb6829d8295c97b6143cdb30332a92d
김지연 <90545043+kdelay@users.noreply.github.com>
2024-09-06 16:06:22 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.34 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        String[] str = my_string.replaceAll("[^0-9]", ",").split(",");
+        for (String s : str) {
+            if (!s.isEmpty()) answer += Integer.parseInt(s);
+        }
+        return answer;
+    }
+}
