a1df2df5117bcc7c3836d701a175be85dc8460ef
김지연 <90545043+kdelay@users.noreply.github.com>
2024-09-11 14:41:57 +0900

    [level 2] Title: 피보나치 수, Time: 2.44 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -4,7 +4,7 @@ class Solution {
         dp[0] = 0;
         dp[1] = 1;
         for (int i = 2; i < dp.length; i++) {
-            dp[i] = ((dp[i-1] % 1234567) + (dp[i-2] % 1234567)) % 1234567;
+            dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
         }
         return dp[n];
     }
