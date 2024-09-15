62e9cf97923f2c38fa12fccee8796cda99e2de3a
김지연 <90545043+kdelay@users.noreply.github.com>
2024-09-15 10:36:32 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 4.21 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -1,28 +1,17 @@
-import java.util.HashSet;
-
-/**
- * 1 배열에서 두 수를 구하는 모든 경우의 수를 구한다.
- * 자신보다 뒤에 있는 수를 선택한다.
- * 2 과정 1에서 구한 수를 새로운 배열에 저장하고 중복값을 제거한다.
- * 3 배열을 오름차순으로 정렬하고 반환한다.
-**/
+import java.util.*;
 
 class Solution {
     public int[] solution(int[] numbers) {
-        HashSet<Integer> set = new HashSet<>(); //중복값 제거를 위한 HashSet
         
-        //두 수를 선택하는 모든 경우의 수
-        for (int i=0; i<numbers.length-1; i++) {
-            for (int j=i+1; j<numbers.length; j++) {
-                //결과 HashSet에 추가
-                set.add(numbers[i] + numbers[j]);
+        Set<Integer> set = new HashSet<>();
+        
+        for (int i = 0; i < numbers.length-1; i++) {
+            for (int j = i+1; j < numbers.length; j++) {
+                int tmp = numbers[i] + numbers[j];
+                //값이 없을 때만 추가
+                if (!set.contains(tmp)) set.add(tmp);
             }
         }
-        
-        //오름차순으로 정렬하고 int[]로 변환하여 반환
-        return set.stream()
-            .sorted()
-            .mapToInt(Integer::intValue)
-            .toArray();
+        return set.stream().sorted().mapToInt(Integer::intValue).toArray();
     }
 }
