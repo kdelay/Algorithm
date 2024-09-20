d5f73105cbbfc4863b9d010e3e49c01bb74da789
김지연 <90545043+kdelay@users.noreply.github.com>
2024-09-20 22:44:23 +0900

    [level 2] Title: 행렬의 곱셈, Time: 11.19 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -1,28 +1,19 @@
-/**
- * 행렬 곱셈 A[m][n] * B[n][k] -> 새로운 행렬 C[m][k]
- * arr1, arr2의 각 행렬 길이를 구한다.
- * 값을 저장할 새로운 행렬을 만든다.
- * arr1 열 길이만큼 * arr2 행 길이만큼 반복하고, 각 수를 곱한 후 더한다.
- */
 class Solution {
     public int[][] solution(int[][] arr1, int[][] arr2) {
-        int r1 = arr1.length;
-        int c1 = arr1[0].length;
-        int r2 = arr2.length;
-        int c2 = arr2[0].length;
-
-        int[][] arr3 = new int[r1][c2];
-
-        for (int i=0; i<r1; i++) {
-
-            //행렬 핵심 로직
-            for (int j=0; j<c2; j++) {
-                for (int k=0; k<c1; k++) {
-                    arr3[i][j] += arr1[i][k] * arr2[k][j];
+        
+        int len1 = arr1.length;
+        int len1Sub = arr1[0].length;
+        int len2 = arr2[0].length;
+        
+        int[][] result = new int[len1][len2];
+        
+        for (int i = 0; i < len1; i++) {
+            for (int j = 0; j < len2; j++) {
+                for (int k = 0; k < len1Sub; k++) {
+                    result[i][j] += (arr1[i][k] * arr2[k][j]);
                 }
             }
-            
         }
-        return arr3;
+        return result;
     }
 }
