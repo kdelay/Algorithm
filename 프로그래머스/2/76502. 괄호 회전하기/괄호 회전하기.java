af0dd9b9f466c0df97f7f8da82e71572db794642
김지연 <90545043+kdelay@users.noreply.github.com>
2024-09-22 22:44:47 +0900

    [level 2] Title: 괄호 회전하기, Time: 124.44 ms, Memory: 95.2 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,49 +1,36 @@
-import java.util.ArrayDeque;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 class Solution {
-    /**
-     * 문자열 회전: 괄호 문자를 왼쪽으로 한 칸씩 밀고, 맨 앞의 괄호 문자를 맨 뒤로 보낸다.
-     * @param s (원본) 괄호 문자열
-     * @return 올바른 괄호 문자열 개수
-     */
     public int solution(String s) {
-        //괄호 짝을 미리 선언해둔다.
-        Map<Character, Character> map = new HashMap<>();
-        map.put(')', '(');
-        map.put(']', '[');
-        map.put('}', '{');
-
-        int length = s.length(); //원본 문자열 길이
-        //회전 대신 문자열의 길이를 늘려서 확인한다.
-        s += s;
-        int answer = 0;
-
-        A: for (int i = 0; i < length; i++) {
-           ArrayDeque<Character> stack = new ArrayDeque<>();
-
-            //s 배열 값을 순회하면서 닫힌 괄호가 있는지 확인한다.
-            for (int j = i; j < i + length; j++) {
-
-                char c = s.charAt(j);
-
-                //열린 괄호: stack에 추가
-                if (!map.containsKey(c)) {
-                    stack.push(c);
-                }
-                //닫힌 괄호: 비교
-                else {
-                    //stack이 비어있거나 짝이 안 맞는 괄호인 경우는 올바른 괄호가 아니다. A로 되돌아간다.
-                    if (stack.isEmpty() || !stack.pop().equals(map.get(c)))
-                        continue A;
-                }
-            }
-            //continue 되지 않고, 스택이 비어있으면 올바른 괄호 문자열이다.
-            if (stack.isEmpty()) {
-                answer++;
+        //deque 초기 값 세팅
+        Deque<Character> deque = new LinkedList<>();
+        for (char c : s.toCharArray()) deque.add(c);
+        
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            //stack이 비어있는 경우 올바른 괄호 문자열 개수 추가
+            if (isCorrect(deque)) count++;
+            deque.addLast(deque.removeFirst());
+        }
+        return count;
+    }
+    
+    private boolean isCorrect(Deque<Character> deque) {
+        
+        Stack<Character> stack = new Stack<>();    
+        for (char d : deque) {
+            //stack이 비어있는 경우, 값을 넣는다.
+            if (stack.isEmpty()) stack.push(d);
+            //stack에 값이 있는 경우
+            else {
+                //올바른 괄호 문자열인지 확인한다.
+                if (stack.peek() == '(' && d == ')') stack.pop();
+                else if (stack.peek() == '[' && d == ']') stack.pop();
+                else if (stack.peek() == '{' && d == '}') stack.pop();
+                //일치하지 않으면 stack에 값을 넣는다.
+                else stack.push(d);
             }
         }
-        return answer;
+        return stack.isEmpty();
     }
 }
