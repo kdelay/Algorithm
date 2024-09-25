a25eff53885de78008935a4f90b144278d8b4bb1
김지연 <90545043+kdelay@users.noreply.github.com>
2024-09-25 13:57:31 +0900

    [level 2] Title: 주식가격, Time: 19.53 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,35 +1,13 @@
-import java.util.*;
-
 class Solution {
-    /**
-     *
-     * @param prices 주식 가격이 담긴 원본 배열
-     * @return 가격이 떨어지지 않은 기간(초)
-     */
     public int[] solution(int[] prices) {
-        ArrayDeque<Integer> stack = new ArrayDeque<>();
-
-        int len = prices.length;
-        int[] answers = new int[len];
-
-        //stack 초기값 설정
-        stack.push(0);
-
-        for (int i = 1; i < len; i++) {
-            //스택을 사용해 이전 가격과 현재 가격 비교
-            while (!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
-                //주식 가격이 떨어진 경우
-                int idx = stack.pop();
-                answers[idx] = i - idx;
-            }
-            stack.push(i);
-        }
         
-        //주식 가격이 떨어지지 않은 경우
-        while (!stack.isEmpty()) {
-            int idx = stack.pop();
-            answers[idx] = len - 1 - idx;
+        int[] answer = new int[prices.length];
+        for (int i = 0; i < prices.length-1; i++) {
+            for (int j = i+1; j < prices.length; j++) {
+                answer[i]++;
+                if (prices[i] > prices[j]) break;
+            }
         }
-        return answers;
+        return answer;
     }
 }
