a2674474804be58a139addf6d608f69f2a8a92a6
김지연 <90545043+kdelay@users.noreply.github.com>
2024-09-26 19:27:26 +0900

    [level 1] Title: 2016년, Time: 0.03 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.java
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -1,7 +1,13 @@
-import java.time.*;
+import java.util.*;
 
 class Solution {
     public String solution(int a, int b) {
-        return LocalDate.of(2016, a, b).getDayOfWeek().toString().substring(0, 3);
+        
+        //요일
+        String[] weekday = new String[]{"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
+        //각 월별 시작 요일
+        int[] start = new int[]{5, 1, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4};
+        
+        return weekday[(start[a - 1] + (b - 1)) % 7];
     }
 }
--- 프로그래머스/1/12901. 2016년/README.md
+++ 프로그래머스/1/12901. 2016년/README.md
@@ -1,10 +1,10 @@
 # [level 1] 2016년 - 12901 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12901?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12901) 
 
 ### 성능 요약
 
-메모리: 4.2 MB, 시간: 0.03 ms
+메모리: 74.6 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 26일 17:10:04
+2024년 09월 26일 19:27:23
 
 ### 문제 설명
 
