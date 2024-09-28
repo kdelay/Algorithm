392d261231a3b5a3a675d5459c8a58014185c68b
김지연 <90545043+kdelay@users.noreply.github.com>
2024-09-28 11:54:19 +0900

    [level 1] Title: 소수 만들기, Time: 3.57 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -4,25 +4,24 @@ class Solution {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 for (int k = j + 1; k < nums.length; k++) {
-                    int tmp = nums[i] + nums[j] + nums[k];
-                    if (isPrime(tmp)) count++;    
+                    count += isPrime(nums[i] + nums[j] + nums[k]);
                 }
             }
         }
         return count;
     }
     
-    private boolean isPrime(int number) {
+    private int isPrime(int number) {
         //1 이하는 소수가 아님
-        if (number <= 1) return false;
+        if (number <= 1) return 0;
         //2 소수
-        if (number == 2) return true;
+        if (number == 2) return 1;
         //짝수는 소수가 아님
-        if (number % 2 == 0) return false;
+        if (number % 2 == 0) return 0;
         //3부터 제곱근까지 확인
         for (int i = 3; i <= Math.sqrt(number); i+= 2) {
-            if (number % i == 0) return false;
+            if (number % i == 0) return 0;
         }
-        return true;
+        return 1;
     }
 }
