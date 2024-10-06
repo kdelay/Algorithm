2cbb0afd01e37bf38faf4e7c23690c11664a3c61
김지연 <90545043+kdelay@users.noreply.github.com>
2024-10-06 12:26:12 +0900

    [Silver IV] Title: 큐, Time: 276 ms, Memory: 19764 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -1,42 +1,57 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 
-class Main {
+public class Main {
+
+    static final int MAX_SIZE = 10000;
+    static int front = -1, rear = -1;
+    static int length = 0;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        Queue<Integer> queue = new LinkedList<>();
-        int last = 0;
+        int size = Integer.parseInt(br.readLine());
 
-        for (int i=0; i<N; i++) {
-            String[] input = br.readLine().split(" ");
-            switch (input[0]) {
+        int[] data = new int[MAX_SIZE];
+
+        for (int i = 0; i < size; i++) {
+            String[] command = br.readLine().split(" ");
+            switch (command[0]) {
                 case "push":
-                    last = Integer.valueOf(input[1]);
-                    queue.add(last);
+                    if (!isFull()) {
+                        rear++;
+                        length++;
+                        data[rear] = Integer.parseInt(command[1]);
+                    }
                     break;
                 case "pop":
-                    if (queue.isEmpty()) { System.out.println(-1); break; }
-                    System.out.println(queue.remove());
-                    break;
-                case "size":
-                    System.out.println(queue.size());
-                    break;
-                case "empty":
-                    System.out.println(queue.isEmpty() ? 1 : 0);
+                    if (!isEmpty()) {
+                        front++;
+                        length--;
+                        System.out.println(data[front]);
+                    } else System.out.println(-1);
                     break;
                 case "front":
-                    if (queue.isEmpty()) { System.out.println(-1); break; }
-                    System.out.println(queue.element());
+                    System.out.println(isEmpty() ? -1 : data[front + 1]);
                     break;
                 case "back":
-                    if (queue.isEmpty()) { System.out.println(-1); break; }
-                    System.out.println(last);
+                    System.out.println(isEmpty() ? -1 : data[rear]);
+                    break;
+                case "size":
+                    System.out.println(isEmpty() ? 0 : length);
+                    break;
+                case "empty":
+                    System.out.println(isEmpty() ? 1 : 0);
                     break;
             }
         }
+    }
+
+    private static boolean isFull() {
+        return rear == MAX_SIZE;
+    }
+
+    private static boolean isEmpty() {
+        return front == rear;
     }
 }
