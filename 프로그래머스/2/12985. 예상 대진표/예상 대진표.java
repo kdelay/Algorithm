5a0620dd55240f0e216a79681c0826849702f859
김지연 <90545043+kdelay@users.noreply.github.com>
2024-11-10 11:49:26 +0900

    [level 2] Title: 예상 대진표, Time: 0.06 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -2,12 +2,11 @@ class Solution
 {
     public int solution(int n, int a, int b)
     {
-        int answer;
-        
-        for (answer = 0; a != b; answer++) {
-            a = (a+1) / 2;
-            b = (b+1) / 2;
+        int level;
+        for (level = 0; a != b; level++) {
+            a = (a + 1) / 2;
+            b = (b + 1) / 2;
         }
-        return answer;
+        return level;
     }
 }
