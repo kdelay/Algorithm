03b7e1104c183556b1cfd3c3e5de8c3e58a5fa89
김지연 <90545043+kdelay@users.noreply.github.com>
2024-11-30 01:31:05 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 72.07 ms, Memory: 140 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -1,45 +1,43 @@
 import java.util.*;
 
 class Solution {
-    /**
-     * @param enroll 판매원 이름 배열 (center 제외)
-     * @param referral 다단계 조직에 참여시키 다른 판매원의 이름 배열
-     * @param seller 판매량 집계 데이터 - 판매원 이름 배열
-     * @param amount 판매량 집계 데이터 - 판매 수량 배열
-     * @return
-     */
-    public static int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
+    
+    static Map<String, String> relationMap; //다단계 관계
+    static Map<String, Integer> money; //이익 금액
+    
+    public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
 
-        //부모-자식 관계 해시맵 생성
-        HashMap<String, String> parent = new HashMap<>();
+        //초기 값
+        relationMap = new HashMap<>();
+        money = new HashMap<>();
         for (int i = 0; i < enroll.length; i++) {
-            parent.put(enroll[i], referral[i]);
-        }
-
-        //total 금액 해시맵 생성
-        HashMap<String, Integer> total = new HashMap<>();
-
-        //이익 분배
-        for (int i = 0; i < seller.length; i++) {
-            String curName = seller[i]; //판매자
-            int money = amount[i] * 100; //판매자가 판매한 총 금액
-
-            //판매자로부터 차례대로 상위 노드로 이동하며 이익 분배
-            while (money > 0 && !curName.equals("-")) {
-                //현재 판매자가 받을 금액 계산(10%를 제외한 금액)
-                total.put(curName, total.getOrDefault(curName, 0) + money - (money / 10));
-                curName = parent.get(curName);
-                
-                //10%를 제외한 금액 계산
-                money /= 10;
-            }
+            relationMap.put(enroll[i], referral[i]);
+            money.put(enroll[i], 0);
         }
         
-        //모든 노드에 대해 해당하는 이익을 배열로 반환
+        //추천인에게 각각 10%씩 이익 분배
+        for (int i = 0; i < seller.length; i++) {
+            divisionAmount(seller[i], amount[i] * 100);
+        }
+        
+        //결과 배열 생성
         int[] answer = new int[enroll.length];
         for (int i = 0; i < enroll.length; i++) {
-            answer[i] = total.getOrDefault(enroll[i], 0);
+            answer[i] = money.get(enroll[i]);
         }
         return answer;
+    }
+    
+    private void divisionAmount(String name, int amount) {
+        
+        //이익 분배
+        int tenPercent = (int)((double)amount * 0.1);
+        money.put(name, (money.get(name) + amount - tenPercent));
+        
+        //종료 조건: "-" 이거나, 금액이 1원 미만이면 종료
+        if (relationMap.get(name).equals("-") || amount < 1) return;
+        
+        //최상단까지 재귀 진행
+        divisionAmount(relationMap.get(name), tenPercent);
     }
 }
