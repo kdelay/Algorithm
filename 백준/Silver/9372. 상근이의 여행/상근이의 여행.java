c217f088244939961e1122c74975e55b742890ab
김지연 <90545043+kdelay@users.noreply.github.com>
2024-11-30 15:37:23 +0900

    [Silver IV] Title: 상근이의 여행, Time: 188 ms, Memory: 24060 KB -BaekjoonHub

--- 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
@@ -1,27 +1,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        //테스트 케이스
-        int T = Integer.parseInt(br.readLine());
-        for (int i = 0; i < T; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int N = Integer.parseInt(st.nextToken()); //국가의 수
-            int M = Integer.parseInt(st.nextToken()); //비행기 종류
+        int T = Integer.parseInt(br.readLine()); //테스트 케이스 수
 
-            //왕복 비행 스케줄
-            for (int j = 0; j < M; j++) {
-                st = new StringTokenizer(br.readLine());
-            }
-            
+        StringBuilder sb = new StringBuilder(); //결과 한 번에 모아서 출력
+        while (T-- > 0) {
+            String[] input = br.readLine().split(" ");
+            int N = Integer.parseInt(input[0]); //국가 수
+            int M = Integer.parseInt(input[1]); //비행기 수
+
+            //비행기 정보를 읽을 필요가 없음
+            for (int j = 0; j < M; j++) br.readLine();
+
             //주어지는 비행 스케줄은 항상 연결 리스트이므로, 최대 개수는 N-1 개이다.
-            System.out.println(N-1);
+            sb.append(N - 1).append("\n");
         }
+        System.out.println(sb);
     }
 }
