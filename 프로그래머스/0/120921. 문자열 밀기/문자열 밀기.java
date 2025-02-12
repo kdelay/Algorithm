78c5837b7f7b30b8162f31fd249095959c86ceda
김지연 <90545043+kdelay@users.noreply.github.com>
2025-02-12 14:53:37 +0900

    [level 0] Title: 문자열 밀기, Time: 14.67 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
@@ -1,32 +1,14 @@
 class Solution {
-        public int solution(String A, String B) {
-            if (A.equals(B)) return 0;
-            
-            int len = A.length();
-            String rotated = A;
-            
-            for (int i = 1; i < len; i++) {
-                rotated = rotated.charAt(len - 1) + rotated.substring(0, len - 1);
-                if (rotated.equals(B)) return i;
-            }
-            return -1;
-        }
-    
-/**
     public int solution(String A, String B) {
-        int rotation = 0, len = A.length();
-        String[] aList = A.split("");
+        if (A.equals(B)) return 0;
 
-        for (int i = 0; i < len; i++) {
-            if (String.join("", aList).equals(B)) return rotation;
+        int len = A.length();
+        String rotated = A;
 
-            String last = aList[len - 1];
-
-            for (int j = len - 1; j > 0; j--) aList[j] = aList[j - 1];
-            aList[0] = last;
-            rotation++;
+        for (int i = 1; i < len; i++) {
+            rotated = rotated.charAt(len - 1) + rotated.substring(0, len - 1);
+            if (rotated.equals(B)) return i;
         }
-        return rotation == len ? -1 : rotation;
+        return -1;
     }
-**/
 }
