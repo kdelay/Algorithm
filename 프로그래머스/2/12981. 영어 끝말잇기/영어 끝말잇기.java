ad9f8817d9a3b28244ccd7e177717af80936c739
김지연 <90545043+kdelay@users.noreply.github.com>
2025-04-06 11:26:12 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.07 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,26 +1,17 @@
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 class Solution {
     public int[] solution(int n, String[] words) {
-
         Set<String> set = new HashSet<>();
-        char preWord = words[0].charAt(0);
-
+        
         for (int i = 0; i < words.length; i++) {
-
-            //이미 나온 단어를 말하거나 잘못 이어 말한 경우
-            if (set.contains(words[i]) || words[i].charAt(0) != preWord) {
-                return new int[]{(i % n) + 1, (i / n) + 1};
+            if (i > 0 && words[i].charAt(0) != words[i - 1].charAt(words[i - 1].length() - 1)) {
+                return new int[] { (i % n) + 1, (i / n) + 1 };
             }
-            
-            //사용 단어 출가
+            if (set.contains(words[i])) return new int[] { (i % n) + 1, (i / n) + 1 };
             set.add(words[i]);
-            
-            //마지막 글자 업데이트
-            preWord = words[i].charAt(words[i].length() - 1);
         }
-        return new int[]{0, 0};
+        return new int[] { 0, 0 };
     }
 }
