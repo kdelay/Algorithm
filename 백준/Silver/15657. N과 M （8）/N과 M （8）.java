1d6c59fb6dc89fe1f42d111a976b1cf95f378da0
김지연 <90545043+kdelay@users.noreply.github.com>
2025-06-11 18:24:16 +0900

    [Silver III] Title: N과 M (8), Time: 220 ms, Memory: 20780 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class Main {
+    static int N, M;
+    static int[] arr;
+    static int[] result;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        result = new int[M];
+        dfs(0, 0);
+        System.out.println(sb);
+    }
+
+    static void dfs(int depth, int start) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(result[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = start; i < N; i++) {
+            result[depth] = arr[i];
+            dfs(depth + 1, i);
+        }
+    }
+}
