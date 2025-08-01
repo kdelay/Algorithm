3095483efca00f2b8373c5c65f0a52300007feb9
김지연 <90545043+kdelay@users.noreply.github.com>
2025-08-01 21:07:01 +0900

    [level 1] Title: K번째수, Time: 0.50 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,15 +1,32 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
+        int len = commands.length;
+        int[] answer = new int[len];
         
-        int[] answer = new int[commands.length];
-        for (int i = 0; i < commands.length; i++) {
-            //범위 만큼 자름
-            int[] tmp = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
-            Arrays.sort(tmp); //오름차순 정렬
-            answer[i] = tmp[commands[i][2]-1];
+        for(int i = 0; i < len; i++) {
+            answer[i] = action(array, commands[i]);
+            
         }
         return answer;
+    }
+    
+    
+    
+    public int action(int[] array, int[] command) {
+        int i = command[0];
+        int j = command[1];
+        int k = command[2];
+        int len = j - i + 1;
+        int[] res = new int[len];
+        
+        for(int a = 0; a < len; a++) {
+            res[a] = array[a + (i - 1)];
+        }
+        
+        Arrays.sort(res);
+        return res[k - 1];
+        
     }
 }
