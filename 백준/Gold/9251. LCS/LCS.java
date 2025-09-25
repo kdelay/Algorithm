46ae985a9e6736ab7f8c98ae6e2cbc3a13f42c71
김지연 <90545043+kdelay@users.noreply.github.com>
2025-09-26 01:33:25 +0900

    [Gold V] Title: LCS, Time: 140 ms, Memory: 19032 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -1,29 +1,24 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      char[] a = br.readLine().toCharArray();
-      char[] b = br.readLine().toCharArray();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s1 = br.readLine();
+        String s2 = br.readLine();
 
-      int lenA = a.length;
-      int lenB = b.length;
-      int[][] dp = new int[lenA+1][lenB+1];
+        int len1 = s1.length();
+        int len2 = s2.length();
+        int[][] dp = new int[len1 + 1][len2 + 1];
 
-      for (int i=1; i<=lenA; i++) {
-        for (int j=1; j<=lenB; j++) {
-          if (a[i-1] == b[j-1]) { //동일한 문자일 경우 대각선 값+1 저장
-            dp[i][j] = dp[i-1][j-1] + 1;
-          } else { //다른 문자일 경우 상좌 데이터 중 큰 수 저장
-            dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
-          }
+        for (int i = 1; i <= len1; i++) {
+            for (int j = 1; j <= len2; j++) {
+                dp[i][j] = (s1.charAt(i - 1) == s2.charAt(j - 1))
+                    ? dp[i - 1][j - 1] + 1
+                    : Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
         }
-      }
-      System.out.println(dp[lenA][lenB]);
+        System.out.println(dp[len1][len2]);
     }
 }
